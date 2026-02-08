e06be8cfe51bad2f4d878fb12e7cfe30d8679653
h2cone <tw8ape@gmail.com>
2026-02-08 14:06:39 +0800

    Improve Wikipedia URL extraction robustness

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,24 @@
 const wikiUrl = 'https://en.wikipedia.org/wiki/Z-Library';
-const selector = '#mw-content-text > div.mw-content-ltr.mw-parser-output > table.infobox.vcard > tbody > tr:nth-child(4) > td > div > ul > li:nth-child(2) > span > a'
+const selectors = [
+    '#mw-content-text > div.mw-content-ltr.mw-parser-output > table.infobox.vcard > tbody > tr:nth-child(4) > td > div > ul > li:nth-child(2) > a',
+    '#mw-content-text > div.mw-content-ltr.mw-parser-output > table.infobox.vcard > tbody > tr:nth-child(4) > td > div > ul > li:nth-child(2) > span > a',
+    'table.infobox.vcard a.external'
+];
 
 fetch(wikiUrl)
     .then(response => response.text())
     .then(text => {
         let parser = new DOMParser();
         let doc = parser.parseFromString(text, 'text/html');
-        // select the target element
-        let target = doc.querySelector(selector);
-        let url = target.getAttribute('href');
+        let target = selectors
+            .map(selector => doc.querySelector(selector))
+            .find(element => element !== null);
+
+        if (!target) {
+            throw new Error('Unable to find Z-Library URL from Wikipedia infobox.');
+        }
+
+        let url = new URL(target.getAttribute('href'), wikiUrl).href;
         // open the url in a new tab
         chrome.tabs.create({ url: url });
     })
